188039e2844ae415c718470f681fa937aa878eea
Dhananjay kumar <107258319+dhananjaykr306@users.noreply.github.com>
2024-11-22 05:26:27 +0530

    [Dhananjay] add : Logger file containing logger function

---
+++ logger.py
@@ -0,0 +1,28 @@
+'''
+    @Author: Dhananjay Kumar
+    @Date: 04-11-2024
+    @Last Modified by: Dhananjay Kumar
+    @Last Modified time: 04-11-2024
+    @Title : Logger file containing logger function
+
+'''
+
+import logging
+
+def logger_init(name):
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    file_handler = logging.FileHandler('all.log')
+
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
